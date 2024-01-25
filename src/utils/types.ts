5f6d474f2fc50b24dfc070c870fd0821626d19e3
Joseph Bouqdib <joseph@bouqdib.com>
2024-01-25 14:04:19 +0000

    Add GoogleMapDisplayProps interface

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,19 +1,25 @@
-export type GeoLocation = {
+export interface GeoLocation {
   latitude: number;
   longitude: number;
-};
+}
 
-export type SearchOptions = {
+export interface SearchOptions {
   textQuery: string;
   maxResultCount: number;
   locationBias?: object;
-};
+}
 
-export type SearchResult = {
+export interface SearchResult {
   displayName: { text: string };
   formattedAddress: string;
   id: string;
   location: GeoLocation;
   rating: number;
   types: string[];
-};
+}
+
+export interface GoogleMapDisplayProps {
+  latitude: number;
+  longitude: number;
+  searchResult?: SearchResult[];
+}
