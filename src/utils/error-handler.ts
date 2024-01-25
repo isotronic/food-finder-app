d16de214c548cf458eda0321cb0a7d0c9de0b8d3
Joseph Bouqdib <joseph@bouqdib.com>
2024-01-25 14:03:46 +0000

    Check for GeolocationPositionError

--- src/utils/error-handler.ts
+++ src/utils/error-handler.ts
@@ -1,4 +1,5 @@
 export function getErrorMessage(error: unknown) {
   if (error instanceof Error) return error.message;
+  if (error instanceof GeolocationPositionError) return error.message;
   return String(error);
 }
