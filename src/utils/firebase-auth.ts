6e354ac56528291c3acd4d21077d1d3058fe0a9f
Joseph Bouqdib <joseph@bouqdib.com>
2024-01-30 12:31:06 +0000

    Add create, login and logout user with firebase

---
+++ src/utils/firebase-auth.ts
@@ -0,0 +1,29 @@
+import { firebaseApp } from "./firebase-setup";
+import {
+  browserLocalPersistence,
+  createUserWithEmailAndPassword,
+  getAuth,
+  setPersistence,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { AuthenticationFormValues } from "./types";
+
+export const firebaseAuth = getAuth(firebaseApp);
+
+setPersistence(firebaseAuth, browserLocalPersistence);
+
+export async function createUser({ email, password }: AuthenticationFormValues) {
+  const result = await createUserWithEmailAndPassword(firebaseAuth, email, password);
+  return result;
+}
+
+export async function loginUser({ email, password }: AuthenticationFormValues) {
+  const result = await signInWithEmailAndPassword(firebaseAuth, email, password);
+  return result;
+}
+
+export async function logoutUser() {
+  const result = await signOut(firebaseAuth);
+  return result;
+}
