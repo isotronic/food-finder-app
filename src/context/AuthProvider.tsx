6acde5a2204aadbfc78dbc3fc883f14000e600a8
Joseph Bouqdib <joseph@bouqdib.com>
2024-01-30 12:32:30 +0000

    Add Context and Provider for authentication

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,7 +1,9 @@
-import { useState, useEffect } from "react";
-import { firebaseAuth } from "../utils/firebase";
-import { AuthContext } from "./AuthContext";
+import { useState, useEffect, createContext } from "react";
+import { firebaseAuth } from "../utils/firebase-auth";
 import { User, onAuthStateChanged } from "firebase/auth";
+import { AuthContextProps } from "../utils/types";
+
+export const AuthContext = createContext({} as AuthContextProps);
 
 export default function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
--- src/store/auth-context.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import { AuthContextProps } from "../utils/types";
-
-export const AuthContext = createContext({} as AuthContextProps);
